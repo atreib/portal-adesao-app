1c57729e1e35c752c8999022486e794b8158bcda
atreib <andre.schumacher.treib@gmail.com>
2021-10-30 23:52:22 -0300

    Implement auto tailwind css generation

---
+++ pages/login.tsx
@@ -0,0 +1,10 @@
+const Login = () => {
+  return (<>
+    <div className='fixed top-0 right-0 w-full h-full bg-gray-700' aria-hidden="true" />
+    <div className='relative min-h-screen text-white'>
+      Logindsfdsfs
+    </div>
+  </>);
+};
+
+export default Login;
